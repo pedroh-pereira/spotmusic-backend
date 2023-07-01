a99be0137703c2ceea3fac4337b559d7a967ea32
plinioduartt <plinio.duartes@hotmail.com>
2023-07-01 15:08:56 -0300

    fix: change unit test

--- test.py
+++ test.py
@@ -8,12 +8,8 @@ class Test(unittest.TestCase):
         self.app = app.test_client()
 
         # envia uma requisicao GET para a URL
-        self.result = self.app.get('/healthcheck')
+        self.result = self.app.get('/')
 
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
-
-    def test_conteudo(self):
-        # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
